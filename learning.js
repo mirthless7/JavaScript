83ee5930daba3a39aa3da129615023a438485cc2
Bryan <bryanleyva2005@gmail.com>
2023-10-09 23:01:13 -0400

    strings

---
+++ learning.js
@@ -0,0 +1,27 @@
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//Example of const and let
+const FCC = "freeCodeCamp"; 
+let fact = "is cool!"; 
+fact = "is awesome!";
+console.log(FCC, fact); 
+
+//Arthimetic operators
+const quotient=45/12
+const product=45*2
+const sum=2+2
+const difference=2-1
+//% is remainder 5%2=1
+//i++ is the same as i=i+1
+//i-- is the same as i=i-1
+
+console.log(sum)
+
